d83632f74a644e10d640214bbf93e72381bc7c45
apinanyogaratnam <apinan@ontopical.com>
2023-07-09 16:40:45 -0400

    feat: generating jwt token

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -1,14 +1,38 @@
-package jwt;
+package jwt
 
 import (
 	"log"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"golang.org/x/net/context"
 )
 
 type Server struct {}
 
+func GenerateJWT(userId int) (string, error) {
+	secretKey := "secret"
+
+	tokenContent := jwt.MapClaims{
+		"userId": userId,
+		"exp": time.Now().Add(time.Hour * 24).Unix(), // The token will expire after 24 hours
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, tokenContent)
+	signedToken, err := token.SignedString([]byte(secretKey))
+
+	if err != nil {
+		return "", err
+	}
+
+	return signedToken, nil
+}
+
 func (*Server) GetToken(ctx context.Context, message *JWTRequest) (*JWTResponse, error) {
 	log.Println("Received message body from client: ", message.Id);
-	return &JWTResponse{Token: "Hello from the server!"}, nil;
+	token, err := GenerateJWT(int(message.Id))
+	if err != nil {
+		return nil, err
+	}
+	return &JWTResponse{Token: token}, nil;
 }
